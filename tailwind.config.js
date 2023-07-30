fcaf055099b5c1919d94b1181c38d033bd9b7683
SelrahcD <c.desneuf@gmail.com>
2023-07-30 18:24:15 +0200

    Change police

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  content: [
-    './src/**/*.{njk,md}',
-  	'./src/**/*.svg',
-  ],
+	content: [
+		'./src/**/*.{njk,md}',
+		'./src/**/*.svg',
+	],
+	theme: {
+		fontFamily: {
+			sans: [
+				"IBM Plex Sans, sans-serif",
+				{
+					fontFeatureSettings: '"liga","tnum","case","calt","zero","ss01","locl","calt"',
+					fontVariationSettings: '"opsz" 32'
+				},
+			],
+		},
+	},
 }
